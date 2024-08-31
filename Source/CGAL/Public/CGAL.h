7b5b8a09e3ba8dd0cf4c992a42a0b06c04109ce5
allen <allen_lei@yeah.net.com>
2024-08-31 16:57:04 +0800

    feat ： 升级到ue5

--- Source/CGAL/Public/CGAL.h
+++ Source/CGAL/Public/CGAL.h
@@ -1,19 +1,23 @@
 
 #pragma once
 
-#include "ModuleManager.h"
+#include "Modules/ModuleManager.h"
 
 
 
 class FCGALModule : public IModuleInterface
 {
 public:
-
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 
+	/** Handle to the test dll we will load */
+	void *GetLIBGMP_LibraryHandle() { return LIBGMP_LibraryHandle; }
+	void *GetLIBMPFR_LibraryHandle() { return LIBMPFR_LibraryHandle; }
+
 private:
 	/** Handle to the test dll we will load */
-	void*	LibraryHandle;
+	void *LIBGMP_LibraryHandle;
+	void *LIBMPFR_LibraryHandle;
 };
